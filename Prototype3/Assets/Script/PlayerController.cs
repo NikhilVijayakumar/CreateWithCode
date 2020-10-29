44627523a143f4d88b5bb90163add516350fdad9
Nikhil V <nikhil.vijaykumar@gmail.com>
2020-10-30 01:03:35 +0530

    prototype3

---
+++ Prototype3/Assets/Script/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    private float jumpForce = 10;
+    private float gravityModifier = 1.5f;
+    private bool isGrounded = true;
+    public bool gameover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            playerRB.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameover = true;
+            Debug.Log("Game over !! !!");
+        }
+
+    }
+}
