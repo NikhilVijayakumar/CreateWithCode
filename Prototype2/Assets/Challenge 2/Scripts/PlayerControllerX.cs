0c322441a30b649917ffcf9e9b27a09f9a62f4a2
Nikhil V <nikhil.vijaykumar@gmail.com>
2020-10-28 23:51:56 -0700

    challenge 2

---
+++ Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab; 
+    private float spawnInterval = 1.5f;
+    private bool isDogSpawn = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (isDogSpawn && Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(SpawnDog());
+        }       
+    }
+    IEnumerator SpawnDog()
+    {
+        isDogSpawn = false;
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        yield return new WaitForSeconds(spawnInterval);
+        isDogSpawn = true;
+        yield return null;
+    }
+}
