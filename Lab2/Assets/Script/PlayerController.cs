e752219ab363a8bac71f291726375117a107699c
Nikhil V <nikhil.vijaykumar@gmail.com>
2020-10-30 20:02:38 +0530

    Lab2

---
+++ Lab2/Assets/Script/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 10;
+    private float topBound = -10.3f;
+    private float bottomBound = 17.2f;
+    private float leftBound = 21.5f;
+    private float rightBound = -20.1f;
+    private float rebound = 15.1f;
+
+    private Rigidbody playerRB;
+
+   
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+  
+    void Update()
+    {
+        MovePlayer(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        CheckBound();
+    }
+
+    private void MovePlayer(float horizontal, float vertical)
+    {
+        playerRB.AddForce(Vector3.forward * speed * -vertical,ForceMode.Acceleration);
+        playerRB.AddForce(Vector3.right * speed * -horizontal, ForceMode.Acceleration);
+    }
+
+    private void CheckBound()
+    {
+        if(transform.position.z > bottomBound)
+        {
+           transform.position = new Vector3(transform.position.x, transform.position.y, bottomBound);
+            playerRB.AddForce(Vector3.forward *  -rebound, ForceMode.Impulse);
+        }
+       else if (transform.position.z < topBound)
+        {
+           transform.position = new Vector3(transform.position.x, transform.position.y, topBound);
+           playerRB.AddForce(Vector3.forward *  rebound, ForceMode.Impulse);
+        }
+
+        if (transform.position.x > leftBound)
+        {
+            transform.position = new Vector3(leftBound, transform.position.y, transform.position.z);
+            playerRB.AddForce(Vector3.right * -rebound, ForceMode.Impulse);
+        }
+        else if (transform.position.x < rightBound)
+        {
+            transform.position = new Vector3(rightBound, transform.position.y, transform.position.z);
+            playerRB.AddForce(Vector3.right * rebound, ForceMode.Impulse);
+        }
+
+    }
+
+    
+}
