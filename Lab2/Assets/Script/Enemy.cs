bc474abe2a5233b30e676b738d436668929ad45e
Nikhil V <nikhil.vijaykumar@gmail.com>
2020-11-02 19:50:21 +0530

    LAb4

---
+++ Lab2/Assets/Script/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10;
+    private Rigidbody enemyRB;
+    private float bottomBound = 17.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemy();
+        isEnemyAlive();
+       
+    }
+
+    private void isEnemyAlive()
+    {
+        if (transform.position.z > bottomBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void MoveEnemy()
+    {
+        enemyRB.AddForce(Vector3.forward * speed,ForceMode.Acceleration);
+    }
+}
---
+++ Lab2/Assets/Script/SpawnManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemyList;
+    public GameObject powerups;
+    private float startTime = 1f;
+    private float enemyWaveTime = 3.2f;
+    private float powerupTime = 5.5f;
+
+    private float zEnemy = -9.3f;
+    private float zPowerups = 3.3f;
+    private float xSpawn = 19f;
+    private float ySpawn = 0.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("GenerateRandomEnemy", startTime, enemyWaveTime);
+        InvokeRepeating("GenerateRandomPowerups", startTime, powerupTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GenerateRandomEnemy()
+    {
+        int index = Random.Range(0, enemyList.Length);
+        GameObject enemy = enemyList[index];
+        float xEnemy = Random.Range(-xSpawn, xSpawn);
+        Vector3 postion = new Vector3(xEnemy, ySpawn, zEnemy);
+        Instantiate(enemy, postion, enemy.transform.rotation);
+    }
+
+    private void GenerateRandomPowerups()
+    {
+        float xPowerup = Random.Range(-xSpawn, xSpawn);
+        float zSpawn = Random.Range(-zPowerups, zPowerups);
+        Vector3 postion = new Vector3(xPowerup, ySpawn, zSpawn);
+        Instantiate(powerups, postion, powerups.transform.rotation);
+    }
+}
