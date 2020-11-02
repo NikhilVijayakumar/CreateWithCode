ee45aba5b5d39ec664506780eb9c8c4c9dae1586
Nikhil V <nikhil.vijaykumar@gmail.com>
2020-11-02 13:09:06 +0530

    proto 4 complete

---
+++ Prototypr4/Assets/Script/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float speed = 5.0f;
+    private Rigidbody enemyRB;  
+    private SpawnManager manager;
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody>();
+        manager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemy();       
+    }
+
+    private void isEnemyAlive()
+    {
+       if(transform.position.y < -5)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void MoveEnemy()
+    {
+        Vector3 moveTo = (manager.player.transform.position - transform.position).normalized;
+        enemyRB.AddForce(moveTo * speed);
+        isEnemyAlive();
+    }
+}
